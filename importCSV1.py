d50026d35541103092fac30cee4f5cd73fb53a19
Jerdonh <jerdonhelgeson@gmail.com>
2017-11-08 13:38:07 -0800

    namedtuple+notes
    tested a named tuple to import into named TestRecord also included comments on how it works.
    
    MUST INCLUDE IMPORT FROM COLLECTIONS TO USE NAMEDTUPLE

--- importCSV1.py
+++ importCSV1.py
@@ -1,11 +1,19 @@
 #Jerdon Helgeson
 #import from csv 1
 
+from collections import namedtuple
 
-TestRecord = namedtuple('Record', 'name, age, title, department, paygrade')
+#will be using a namedTuple as an object to hold each line of the csv we read in
+# tuple > dict because immutable and clean
+#namedtuple data can be accessed by indexing: tR[0] OR by attribute name: tR.name
+
+TestRecord = namedtuple('TestRecord', ['name', 'age', 'ID','other'])
+tR = TestRecord("Jerdon",22,1234,"I like long walks on the beach and pina coladas")
 
 def testNamedTuple(tR):
     #as the name states... this will test the namedTuple
+    return 0
+    
 
 def import_print():
     import csv
@@ -14,12 +22,13 @@ def import_print():
         for row in reader:
             print(row)
 
-def build_record:
+def build_record():
     #this will create an arbitrary namedtuple(struct) based on the first line of the csv
+    return 0
     
+def create_grid():
+    return 0
     
-def create_grid:
-    
-
+#if __name__ == "__main__":
 def main():
     import_print()
