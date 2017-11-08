609340da17a8fdbadc9fc0d231888764320d0643
Jerdonh <jerdonhelgeson@gmail.com>
2017-11-08 02:30:43 -0800

    starterFunctions
    Some basic starter functions and skeleton

--- importCSV1.py
+++ importCSV1.py
@@ -2,12 +2,24 @@
 #import from csv 1
 
 
+TestRecord = namedtuple('Record', 'name, age, title, department, paygrade')
+
+def testNamedTuple(tR):
+    #as the name states... this will test the namedTuple
+
 def import_print():
     import csv
     with open('MPCI.csv', 'r') as csvfile:
         reader = csv.reader(csvfile, delimiter=',', quotechar='|')
         for row in reader:
             print(row)
+
+def build_record:
+    #this will create an arbitrary namedtuple(struct) based on the first line of the csv
+    
+    
+def create_grid:
+    
 
 def main():
     import_print()
